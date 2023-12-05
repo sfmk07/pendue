70dc32fd305535088a36b70e9e6f8d110ce76641
sfmk07 <151550819+sfmk07@users.noreply.github.com>
2023-12-05 11:22:15 +0100

    commit4

--- jeuxpendue.py
+++ jeuxpendue.py
@@ -29,36 +29,7 @@ while True:
             # Ajout de la lettre à la liste des lettres trouvées
             lettres_trouvees += proposition
             print("-> Bien vu!")
-        else:
+       else:
             # Réduction du nombre de tentatives en cas d'erreur
             tentatives_restantes -= 1
             print("-> Nope\n")
-            
-            # Affichage du pendu progressif
-            if tentatives_restantes == 0:
-                print(" ==========Y= ")
-            if tentatives_restantes <= 1:
-                print(" ||/       |  ")
-            if tentatives_restantes <= 2:
-                print(" ||        0  ")
-            if tentatives_restantes <= 3:
-                print(" ||       /|\\ ")
-            if tentatives_restantes <= 4:
-                print(" ||       / \\ ")
-            if tentatives_restantes <= 5:
-                print("/||           ")
-            if tentatives_restantes <= 6:
-                print("==============\n")
-
-        # Mise à jour de l'affichage du mot en fonction des lettres trouvées
-        affichage_mot = " ".join([lettre + " " if lettre in lettres_trouvees else "_ " for lettre in mot_a_deviner])
-
-        # Vérification si toutes les lettres ont été trouvées
-        if "_" not in affichage_mot:
-            print(f">>> Gagné! Le mot était : {mot_a_deviner} <<<")
-            break
-
-    print("\n    * Fin de la partie *    ")
-    rejouer = input("Voulez-vous rejouer ? (O/N) ").lower()
-    if rejouer != 'o':
-        break
