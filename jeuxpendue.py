2dd2a63a9850d7202b0cd936ba5db84e82a641d1
sfmk07 <151550819+sfmk07@users.noreply.github.com>
2023-12-05 11:18:33 +0100

    sauvegardeé

--- jeuxpendue.py
+++ jeuxpendue.py
@@ -11,3 +11,15 @@ informations_joueur = {
     'defaites': 0,
 }
 
+print(">> Bienvenue dans le pendu <<")
+
+while True:
+    # Sélection aléatoire d'un mot à deviner
+    mot_a_deviner = random.choice(mots)
+    
+    # Initialisation de l'affichage du mot avec des underscores
+    affichage_mot = "_ " * len(mot_a_deviner)
+    
+    # Initialisation de la liste des lettres déjà trouvées
+    lettres_trouvees = ""
+    
